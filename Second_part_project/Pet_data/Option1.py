738b05f3d9ed62921ea6e90d671c94d812b013f1
ortillion <id2301@gmail.com>
2025-02-27 11:34:23 +0100

    option 1

---
+++ Second_part_project/Pet_data/Option1.py
@@ -0,0 +1,8 @@
+# Sur dog data
+import pandas as pd
+data_dog = pd.read_excel("VYE15004.xlsx")
+
+print(data_dog.head())
+
+
+
--- Second_part_project/Pet_data/Pet_data.py
+++ Second_part_project/Pet_data/Pet_data.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 # Charger le fichier Excel
 data_cat = pd.read_excel("AMA08001.xlsx")
-data_dog= pd.read_excel("VYE15004.xlsx")
+data_dog = pd.read_excel("VYE15004.xlsx")
 # Afficher les premières lignes
+
 print(data_dog.head())
 
 print(data_cat.head())
