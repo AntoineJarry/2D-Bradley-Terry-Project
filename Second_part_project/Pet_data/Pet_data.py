0d46df6d20030624f3fc1c9ad41f828c8f12befe
Antoine JARRY <antoinejarry.pro@gmail.com>
2025-02-28 17:29:19 +0100

    apply reduc

--- Second_part_project/Pet_data/Pet_data.py
+++ Second_part_project/Pet_data/Pet_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pickle
 
 # Charger le fichier Excel
 data_cat = pd.read_excel("AMA08001.xlsx")
@@ -16,16 +17,21 @@ data_dog["Loser"] = data_dog.apply(lambda row: row["ALIMENT_A_LIBELLE"] if row["
 # Obtenir la liste unique des croquettes
 croquettes = list(set(data_dog["Winner"]).union(set(data_dog["Loser"])))
 print(croquettes)
+with open("croquettes_dog.npy", "wb") as f:
+    pickle.dump(croquettes, f)
 
 # Créer une matrice vide avec les croquettes en index et colonnes
 comparison_matrix_dog_opt3 = pd.DataFrame(0, index=croquettes, columns=croquettes)
 
 # Remplir la matrice avec le nombre de victoires
 for _, row in data_dog.iterrows():
-    comparison_matrix_dog_opt3.loc[row["Winner"], row["Loser"]] += 1 # Ajoute 1 lorsque l'un est préféré à l'autre
+    comparison_matrix_dog_opt3.loc[row["Winner"], row["Loser"]] += 1  # Ajoute 1 lorsque l'un est préféré à l'autre
 
-# Afficher la matrice
-print(comparison_matrix_dog_opt3)
+# Convertir la matrice pandas en numpy array
+comparison_matrix_dog_opt3_np = comparison_matrix_dog_opt3.to_numpy()
+
+# Afficher la matrice numpy
+print(comparison_matrix_dog_opt3_np)
 
 ## Option 3 cat
 
@@ -36,12 +42,28 @@ data_cat["Loser"] = data_cat.apply(lambda row: row["ALIMENT_A_LIBELLE"] if row["
 croquettes = list(set(data_cat["Winner"]).union(set(data_cat["Loser"])))
 print(croquettes)
 
+# Sauvegarder les matrices numpy dans un fichier pickle
+with open("croquettes_cat.npy", "wb") as f:
+    pickle.dump(croquettes, f)
+
 # Créer une matrice vide avec les croquettes en index et colonnes
 comparison_matrix_cat_opt3 = pd.DataFrame(0, index=croquettes, columns=croquettes)
 
 # Remplir la matrice avec le nombre de victoires
 for _, row in data_cat.iterrows():
-    comparison_matrix_cat_opt3.loc[row["Winner"], row["Loser"]] += 1 # Ajoute 1 lorsque l'un est préféré à l'autre
+    comparison_matrix_cat_opt3.loc[row["Winner"], row["Loser"]] += 1  # Ajoute 1 lorsque l'un est préféré à l'autre
 
-# Afficher la matrice
-print(comparison_matrix_cat_opt3)
+# Convertir la matrice pandas en numpy array
+comparison_matrix_cat_opt3_np = comparison_matrix_cat_opt3.to_numpy()
+
+# Afficher la matrice numpy
+print(comparison_matrix_cat_opt3_np)
+
+# Sauvegarder les matrices numpy dans un fichier pickle
+with open("comparison_matrix_dog_opt3.npy", "wb") as f:
+    pickle.dump(comparison_matrix_dog_opt3_np, f)
+
+with open("comparison_matrix_cat_opt3.npy", "wb") as f:
+    pickle.dump(comparison_matrix_cat_opt3_np, f)
+
+
--- reduction_dim.py
+++ reduction_dim.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import MDS, Isomap, TSNE
 import umap
+import pickle
 from sklearn.decomposition import PCA
 import First_part_project.Bradley_Terry_Model_2D.NR_algorithm.starting_point as starting_point
 import Second_part_project.Bradley_Terry_model_2D.Reduction_model as Reduction_model
@@ -27,10 +28,34 @@ Score_cumules = np.array([
   [22, 7, 21, 20, 1, 14, 15, 16, 0, 13],
   [29, 42, 34, 29, 30, 30, 31, 41, 26, 0]])
 
-dist_matrix = starting_point.matrice_Q(Score_cumules) # Our distance matrix is defined as in the first part of the project
+
+
+# Charger les matrices à partir des fichiers pickle
+with open("comparison_matrix_dog_opt3.npy", "rb") as f:
+    comparison_matrix_dog_opt3 = pickle.load(f)
+
+with open("comparison_matrix_cat_opt3.npy", "rb") as f:
+    comparison_matrix_cat_opt3 = pickle.load(f)
+
+
+# Charger les matrices à partir des fichiers pickle
+with open("dog_mat.npy", "rb") as f:
+    dog_mat = pickle.load(f)
+
+with open("cat_mat.npy", "rb") as f:
+    cat_mat = pickle.load(f)
+
+# Charger les croquettes
+with open("croquettes_dog.npy", "rb") as f:
+    dog_croquettes = pickle.load(f)
+
+with open("croquettes_cat.npy", "rb") as f:
+    cat_croquettes = pickle.load(f)
+
+dist_matrix = starting_point.matrice_Q(comparison_matrix_cat_opt3) # Our distance matrix is defined as in the first part of the project
 ## Maybe later we can try to used another distance because is probably not the best here ?
 Reduction_model.check_distance_matrix(dist_matrix) ## Important to test this and look if we have any problem
-"""
+
 # 1. UMAP
 reducer = umap.UMAP(n_components=2, metric='precomputed',min_dist=0.5,n_neighbors = 3) # Hyper paramètre a tester
 X_umap = reducer.fit_transform(dist_matrix)
@@ -40,26 +65,27 @@ tsne = TSNE(n_components=2, metric="precomputed",perplexity=5,init='random') # H
 X_tsne = tsne.fit_transform(dist_matrix)
 
 # 3. PCA not useful here because PCA need features here. So we will keep four method that we will explain when presenting
-"""
-# 4. Isomap
-isomap = Isomap(n_components=2,n_neighbors=2) # Adjusted parameter 
-X_isomap = isomap.fit_transform(dist_matrix)
 
-"""
+#5. Metric MDS 
+Mmds = MDS(n_components=2, dissimilarity="precomputed",metric=True,n_init = 50,n_jobs = -1,normalized_stress=False) # For compare to our starting point which is a MDS 
+X_Mmds = Mmds.fit_transform(dist_matrix)
+
+
 #6. classical MDS
-mds = starting_point.starting_point(Score_cumules,reverse_v1=False,reverse_v2=False)
+mds = starting_point.starting_point(comparison_matrix_cat_opt3,reverse_v1=False,reverse_v2=False)
+
 
 #7. Non metric MDS 
 
 Nmds = MDS(n_components=2, dissimilarity="precomputed",metric=False,n_init = 50,n_jobs = -1,normalized_stress=True) # For compare to our starting point which is a MDS 
 X_Nmds = Nmds.fit_transform(dist_matrix)
-"""
-teams = np.array(["BRO", "DK", "DRX", "FOX", "GEN", "HLE", "KDF", "KT", "NS", "T1"])
+
+teams = cat_croquettes
 
 # Plotting the results with labels
 fig, axs = plt.subplots(3, 2, figsize=(10, 10))
 
-"""
+
 # UMAP plot
 axs[0, 0].scatter(X_umap[:, 0], X_umap[:, 1])
 for i, label in enumerate(teams):
@@ -71,16 +97,11 @@ axs[0, 1].scatter(X_tsne[:, 0], X_tsne[:, 1])
 for i, label in enumerate(teams):
     axs[0, 1].text(X_tsne[i, 0], X_tsne[i, 1], label, fontsize=9)
 axs[0, 1].set_title("t-SNE - 2D")
-"""
 
-# Isomap plot
-axs[1, 0].scatter(X_isomap[:, 0], X_isomap[:, 1])
-for i, label in enumerate(teams):
-    axs[1, 0].text(X_isomap[i, 0], X_isomap[i, 1], label, fontsize=9)
-axs[1, 0].set_title("Isomap - 2D")
-"""
+
+
 # MDS plot
-mds = np.hstack((mds[0:len(Score_cumules)], mds[len(Score_cumules):]))
+mds = np.hstack((mds[0:len(comparison_matrix_cat_opt3)], mds[len(comparison_matrix_cat_opt3):]))
 axs[1, 1].scatter(mds[:, 0], mds[:, 1])
 for i, label in enumerate(teams):
     axs[1, 1].text(mds[i, 0], mds[i, 1], label, fontsize=9)
@@ -99,7 +120,6 @@ axs[2, 1].scatter(X_Nmds[:, 0], X_Nmds[:, 1])
 for i, label in enumerate(teams):
     axs[2, 1].text(X_Nmds[i, 0], X_Nmds[i, 1], label, fontsize=9)
 axs[2, 1].set_title(" Non-Metric MDS - 2D")
-"""
 
 plt.tight_layout()
 plt.show()
